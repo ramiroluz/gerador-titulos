7adff5c28104099b48ac4c5c8b057c1a998d33a0
Ramiro Batista da Luz <ramiroluz@gmail.com>
2011-10-02 22:30:01 -0300

    Generated titles.

--- gerador_titulos.py
+++ gerador_titulos.py
@@ -2,21 +2,69 @@
 # -*- coding=utf-8
 
 TITLE = 0
-LINE1 = 1
-LINE2 = 2
+SPEAKER = 1
+DATETIME = 2
+
+def break_line(s):
+    lines = []
+    line = ''
+    x = 0
+    for word in s.split():
+        x += len(word) + 1
+        if x > 45:
+            lines.append(line.strip())
+            x = 0
+            line = ''
+        line += ' ' + word
+
+    lines.append(line.strip())
+    if len(lines) == 1:
+        lines.append('')
+    return lines
+
+def parse_palestras(filename='palestras.xml'):
+    import re
+    from BeautifulSoup import BeautifulStoneSoup
+    import HTMLParser
+
+    parser = HTMLParser.HTMLParser()
+
+    palestras_file = open('palestras.xml')
+    palestras_xml = palestras_file.read()
+    palestras_file.close()
+    palestras_soup = BeautifulStoneSoup(palestras_xml,
+            convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
+
+    palestras_itens = palestras_soup.findAll('entry')
+    palestrante_re = re.compile('(?<=<br>Onde:).+',re.M)
+    horario_re = re.compile('\d{2} de out de 2010 \d{2}:\d{2} a \d{2}:\d{2}')
+
+    palestras = []
+    for i in palestras_itens:
+        palestras.append((break_line(parser.unescape(i.title.contents[0])),
+                         palestrante_re.findall(parser.unescape(i.summary.contents[0]))[0],
+                         horario_re.findall(i.summary.contents[0])[0],))
+    return palestras
 
 def process_files(filename, lectures):
     fileobj = file(filename)
-    xml_content = fileobj.read()
+    model_svg = fileobj.read()
     fileobj.close()
     for lecture in lectures:
-        lecture_file_name = lecture[0].replace(' ','_').lower()
-        lecture_file_name = '{0}.svg'.format(lecture_file_name)
-        lecture_file_name = lecture_file_name.replace('_&_','_')
-        title = lecture[TITLE].replace('&','&amp;')
-        xml_content = xml_content.replace('Title',title)
-        xml_content = xml_content.replace('Line1',lecture[LINE1])
-        xml_content = xml_content.replace('Line2',lecture[LINE2])
+        title = [x.replace('&','&amp;') for x in lecture[TITLE]]
+        print ' '.join(title)
+        suffix = title[0][0]
+        lecture_file_name = lecture[DATETIME].replace(' ','_').lower()
+        lecture_file_name = lecture_file_name.replace(':','-').lower()
+        lecture_file_name = '{0}-{1}.svg'.format(suffix,lecture_file_name)
+
+        title[0] = title[0].replace('A: ','')
+        title[0] = title[0].replace('B: ','')
+        xml_content = model_svg.replace('Title',title[0].encode('utf-8'))
+        if len(title) > 1:
+            xml_content = xml_content.replace('Line1',title[1].encode('utf-8'))
+        xml_content = xml_content.replace('Line2',
+                                          lecture[SPEAKER].encode('utf-8'))
         lecture_file_obj = file(lecture_file_name,'w')
         lecture_file_obj.write(xml_content)
         lecture_file_obj.close()
@@ -31,9 +79,7 @@ def command_line_parse():
     return options.filename
 
 def main(filename):
-    lectures = [('Cerimônia de Abertura & Infra-Estrutura DINF C3SL UFPR',
-                 'Luis Carlos Erpen de Bona',
-                 'October 21, 2010, 9:00am – 9:45am')]
+    lectures = parse_palestras()
     process_files(filename,lectures)
 
 if __name__ == '__main__':
---
+++ palestras.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+ts21 = 'Thursday, October 21, 2010'
+s21 = '''8:00am Credenciamento
+9:00am A: Cerimônia de Abertura & Infra-Estrutura DINF C3SL UFPR
+9:45am A: Como me tornei um empreendedor pythonista
+9:45am B: Desenvolvimento Python com MeeGo
+10:30am A: Por que Python?
+10:30am B: TI em saúde: um vácuo de mercado e por que Pythonistas já saem na frente
+11:15am A: Django Design Patterns
+11:15am B: Trading Card Game com Realidade Aumentada
+1:30pm Keynote: The Zen of Startups
+3:00pm A: Metapython Reciclado
+3:00pm B: Python e Segurança da Informação: Desenvolvendo Exploits em Python
+3:45pm A: Desenvolvendo Web Services com App Engine
+3:45pm B: Python Packaging: Passado, Presente e Futuro
+4:30pm A: Python Ágil
+4:30pm B: O Universo revelado com Python
+5:15pm A: Extraindo dados públicos na marra com Python
+5:15pm B: Python em Sistemas Recomendação: A Cobra é Inteligente!
+6:00pm Palestras Relâmpago'''
+ts22 = 'Friday, October 22, 2010'
+s22 = '''9:00am A: Melhorando a performance de projetos Django
+9:00am B: Python e as eleições nas mídias sociais: Como ela ajudou a decidir o Futuro do Brasil
+9:45am A: CartolaFC 2010: Uma aplicação Python rápida e escalável
+9:45am B: Lettuce, BDD fácil e divertido
+10:30am A: Iniciando com Test Driven Development em Django
+10:30am B: Desenvolvendo macros de OpenOffice.org em Python
+11:15am A: Arduino: brincando de eletrônica com Python e hardware livre
+11:15am B: Introdução ao Cálculo Numérico usando SciPy
+1:30pm Keynote: Comunidad, anarquía y subversión
+3:00pm A: Como Fazer Jogos com Python (ou Apresentando a PyGame)
+3:00pm B: Potter vs Voldemort: Lições Ofidiglotas da Prática Pythônica
+3:45pm A: Por que o futuro do Python só depende dos Pythonistas
+3:45pm B: Python + Asterisk: desenvolvendo aplicações de telefonia com StarPy
+4:30pm A: Como não programar em Python ou Python Tricks que você ainda não conhecia
+4:30pm B: MongoDB e Python: Rompendo a barreira do ORM
+5:15pm A: Brasil.Gov: Python Powered EGov
+5:15pm B: Django e CMS customizáveis
+6:00pm Palestras Relâmpago'''
+ts23 = 'Saturday, October 23, 2010'
+s23 = '''9:00am A: Criando um ambiente de desenvolvimento Pythônico
+9:00am B: Implementação do algoritmo de custo mínimo de Dijkstra em Python para calcular a distância mínima entre cidades
+9:45am A: O Zen da programação Python
+9:45am B: Orbited: monitoramento em tempo real
+10:30am A: Pés na Terra, Cabeça nas Nuvens
+10:30am B: Python 3: o que mudou e por que adotar?
+11:15am A: Inteligência Computacional com Python
+11:15am B: Apontador Web Services (para programdores Python, claro!)
+1:30pm Uma Visão do Mundo Ruby
+3:00pm A: Escalando aplicações Django
+3:00pm B: Desenvolvendo soluções comerciais utilizando Open Source
+3:45pm A: Análise de redes sociais usando NetworkX e Google App Engine
+3:45pm B: p14p - Python-on-a-Chip: Embarque este você também
+4:30pm A: Melhores práticas para o desenvolvimento com Plone 4
+4:30pm B: Como utilizar a DAL (Camada de abstração de acesso a dados) do web2py em outros projetos ou frameworks Python
+5:15pm A: Visão Computacional aplicada à Robótica com Python e OpenCV
+5:15pm B: Code Review simplificado através do ReviewBoard
+6:00pm Palestras Relâmpago'''
+
+faltantes = '''9:00am A: Cerimônia de Abertura & Infra-Estrutura DINF C3SL UFPR
+9:45am A: Como me tornei um empreendedor pythonista
+9:45am B: Desenvolvimento Python com MeeGo
+11:15am A: Django Design Patterns
+3:45pm A: Desenvolvendo Web Services com App Engine
+4:30pm B: O Universo revelado com Python
+5:15pm A: Extraindo dados públicos na marra com Python
+6:00pm Palestras Relâmpago
+9:45am B: Lettuce, BDD fácil e divertido
+10:30am A: Iniciando com Test Driven Development em Django
+10:30am B: Desenvolvendo macros de OpenOffice.org em Python
+11:15am A: Arduino: brincando de eletrônica com Python e hardware livre
+11:15am B: Introdução ao Cálculo Numérico usando SciPy
+3:00pm A: Como Fazer Jogos com Python (ou Apresentando a PyGame)
+3:45pm A: Por que o futuro do Python só depende dos Pythonistas
+4:30pm B: MongoDB e Python: Rompendo a barreira do ORM
+5:15pm A: Brasil.Gov: Python Powered EGov
+6:00pm Palestras Relâmpago
+9:00am A: Criando um ambiente de desenvolvimento Pythônico
+9:00am B: Implementação do algoritmo de custo mínimo de Dijkstra em Python para
+calcular a distância mínima entre cidades
+9:45am A: O Zen da programação Python
+9:45am B: Orbited: monitoramento em tempo real
+11:15am B: Apontador Web Services (para programdores Python, claro!)
+3:00pm B: Desenvolvendo soluções comerciais utilizando Open Source
+3:45pm A: Análise de redes sociais usando NetworkX e Google App Engine
+4:30pm A: Melhores práticas para o desenvolvimento com Plone 4
+5:15pm B: Code Review simplificado através do ReviewBoard
+6:00pm Palestras Relâmpago
+'''
+
+print(faltantes)
